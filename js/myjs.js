2e56263f1503e41b1eef476a9b9db8c4a80f7929
Linfue <micanflee@gmail.com>
2016-10-20 18:30:58 +0800

    topic 1020

---
+++ js/myjs.js
@@ -0,0 +1,26 @@
+  $(document).ready(function() {
+    $('#search').focus(function () {
+      $(this).animate({width:'250px'},800);
+    })
+    $('#search').blur(function () {
+      $(this).animate({width:'200px'},800);
+    })
+   $('.view-selector').mouseover(function() {
+   	$(this).popover('show');
+   });
+   $('.view-selector').mouseleave(function () {
+   	$(this).popover('hide');
+   })
+   $('.create-topic a').mouseover(function () {
+   		$(this).css({background:'#4FB7AD'})
+   })
+   $('.create-topic a').mouseleave(function () {
+   		$(this).css({background:'white'})
+   })
+   $('.huoyue a').mouseover(function() {
+   	$(this).popover('show');
+   })
+   $('.huoyue a').mouseleave(function () {
+   	$(this).popover('hide');
+   })
+  });
