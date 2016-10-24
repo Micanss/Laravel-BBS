dcc07eb5fbf5d2d3e9a29a14ae1b3ca8fb24ef97
Linfue <micanflee@gmail.com>
2016-10-24 18:26:28 +0800

    个人资料设置 1024

--- js/myjs.js
+++ js/myjs.js
@@ -17,10 +17,26 @@
    $('.create-topic a').mouseleave(function () {
    		$(this).css({background:'white'})
    })
-   // $('.huoyue a').mouseover(function() {
-   // 	$(this).popover('show');
-   // })
-   // $('.huoyue a').mouseleave(function () {
-   // 	$(this).popover('hide');
-   // })
+   $('textarea').keyup(function() {
+   	// console.log($(this).val)
+   	if ($(this).val().length != 0) {
+   		$('#preview-box').empty();
+	   	$('#preview-box').append(marked($(this).val()));
+	   	$('#preview-box').removeAttr('hidden');
+	} else {
+		$('#preview-box').empty();
+	 	$('#preview-box').attr('hidden',true);
+	}
+   	if ($(this)[0].scrollHeight > 162) {
+   		$(this).css('height',$(this)[0].scrollHeight);
+   	} else {
+   		$(this).css('height','162px');
+   	}
+   	console.log($(this).css('height')+'   '+$(this)[0].scrollHeight);
+   });
+   $('#uploadimg').change(function () {
+   var objUrl =  window.URL.createObjectURL(this.files[0]);
+	console.log("objUrl = "+objUrl) ;
+	$('#preview-img').attr('src',objUrl);
+   })
   });
